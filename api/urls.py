067b3e9a4839b213714a57b94e8dc3a52dc7f81d
CamCoder337 <camcoder337@gmail.com>
2024-01-13 21:05:29 +0100

    add: tokens management

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView, UserLoginView
+from .views import UserRegistrationView, UserLoginView, LogoutView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
     path('login/', UserLoginView.as_view(), name='user-login'),
+    path('logout/', LogoutView.as_view(), name='user-logout'),
 ]
--- api/views.py
+++ api/views.py
@@ -6,6 +6,9 @@ from django.core.mail import send_mail
 from django.conf import settings as st
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseBadRequest
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenViewBase
+
 from .models import CustomUser
 
 
@@ -27,9 +30,13 @@ class UserLoginView(APIView):
                 # Code still valid, user tries to login with the code
                 entered_code = request.data.get('code', '')
                 if entered_code == user.authentication_code:
-                    user.backend = 'django.contrib.auth.backends.ModelBackend'
+                    # Generate a new token JWT
+                    refresh = RefreshToken.for_user(user)
+                    access_token = str(refresh.access_token)
                     login(request, user)
-                    return Response({'detail': 'Logged in successfully.'}, status=status.HTTP_200_OK)
+                    return Response({'detail': 'Logged in successfully.',
+                                     'access_token': access_token,
+                                     }, status=status.HTTP_200_OK)
                 elif entered_code:
                     return Response({'detail': 'Invalid code.'}, status=status.HTTP_400_BAD_REQUEST)
                 else:
@@ -40,3 +47,22 @@ class UserLoginView(APIView):
                 return Response({'detail': 'Authentication code expired. New code sent.'}, status=status.HTTP_200_OK)
         else:
             return HttpResponseBadRequest({'detail': 'User not found.'})
+
+class GetUsers(generics.ListAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+class LogoutView(TokenViewBase, APIView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.data.get("refresh_token")
+
+        if refresh_token:
+            try:
+                # self.blacklist_token(refresh_token)
+                token = RefreshToken(refresh_token)
+                token.blacklist()
+                return Response({"detail": "Logged Out successfully."}, status=status.HTTP_200_OK)
+            except Exception as e:
+                return Response({"detail": "Error during disconnection."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({"detail": "RefreshTocken required to Disconnection."}, status=status.HTTP_400_BAD_REQUEST)
