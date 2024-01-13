0300f7dcf232104fde85fbece12af6c82f39a101
CamCoder337 <camcoder337@gmail.com>
2024-01-13 20:43:49 +0100

    update: login logic

--- api/views.py
+++ api/views.py
@@ -12,30 +12,10 @@ from .models import CustomUser
 class UserRegistrationView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-
-class UserLoginView(APIView):
-    def post(self, request, *args, **kwargs):
-        email = request.data.get('email', '')
-        user = CustomUser.objects.get(email=email)
-        if user:
-            # Generate and send the authentication code by email
-            authentication_code = "123456"  # Replace with your code generation logic
-            send_mail(
-                'Authentication Code',
-                f'Your authentication code is: {authentication_code}',
-                st.EMAIL_HOST_USER,
-                [email],
-                fail_silently=False,
-            )
-
-            # Simulate login for demonstration purposes
-            user.backend = 'django.contrib.auth.backends.ModelBackend'
-            login(request, user)
-
-            return Response({'detail': 'Authentication code sent successfully.'}, status=status.HTTP_200_OK)
-        else:
-            return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
-        
+    def perform_create(self, serializer):
+        # Override perform_create to customize user creation logic if needed
+        user = serializer.save()
+        user.send_authentication_code()
 
 class UserLoginView(APIView):
     def post(self, request, *args, **kwargs):
@@ -50,8 +30,10 @@ class UserLoginView(APIView):
                     user.backend = 'django.contrib.auth.backends.ModelBackend'
                     login(request, user)
                     return Response({'detail': 'Logged in successfully.'}, status=status.HTTP_200_OK)
-                else:
+                elif entered_code:
                     return Response({'detail': 'Invalid code.'}, status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response({'detail': 'Enter a  code.'}, status=status.HTTP_400_BAD_REQUEST)
             else:
                 # Code has expired, send a new one
                 user.send_authentication_code()
