c80117b16e0eba7156d44b349655da93ac0bb3a9
CamCoder337 <camcoder337@gmail.com>
2024-01-15 12:23:39 +0100

    update: moving authentication

--- authentication/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApiAuthentificationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'api.authentification'
-    label = 'api_authentification'
