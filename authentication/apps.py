73753d1f01a47be69780575f5c21300b03026bb1
CamCoder337 <camcoder337@gmail.com>
2024-01-15 11:54:15 +0100

    Refactor code for improved performance

---
+++ authentication/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ApiAuthentificationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api.authentification'
+    label = 'api_authentification'
